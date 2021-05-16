4e046cb3a7bd7dd3e5a28ccfea5e2d120260e803
Dolphiiiin <42102311+Dolphiiiin@users.noreply.github.com>
2021-05-17 01:37:18 +0900

    commit

---
+++ WebView/assets/ad/randomImage.js
@@ -0,0 +1,9 @@
+let imglist = [];
+for(let i=1;i<=14;i++)
+    imglist.push(i);
+console.log(imglist);
+
+let selectnum = Math.floor(Math.random() * imglist.length);
+let output = "<img src=" + imglist[selectnum] + ">";
+
+document.getElementsByClassName("side").appendChild(output);
